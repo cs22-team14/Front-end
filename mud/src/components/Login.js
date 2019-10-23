9dc4d66cb0f4e6270fbade277c6b07d80bbcf4ab
Joel Perez <joelness03@gmail.com>
2019-10-23 09:06:51 -0600

    updated all URLs for internal back-end

--- mud/src/components/Login.js
+++ mud/src/components/Login.js
@@ -1,6 +1,6 @@
 import React from "react";
 import axios from "axios";
-import Navigation from "./Navigation"
+import Navigation from "./Navigation";
 
 import {
   MDBContainer,
@@ -33,10 +33,7 @@ class Login extends React.Component {
   login = e => {
     e.preventDefault();
     axios
-      .post(
-        "https://lambda-mud-test.herokuapp.com/api/login/",
-        this.state.creds
-      )
+      .post("https://adventure14.herokuapp.com/api/login/", this.state.creds)
       .then(res => {
         localStorage.setItem("Token", res.data.key);
         this.props.history.push("/home");
@@ -89,8 +86,8 @@ class Login extends React.Component {
               </MDBCard>
             </MDBCol>
           </MDBRow>
-        </MDBContainer >
-      </div >
+        </MDBContainer>
+      </div>
     );
   }
 }
--- mud/src/utils/axiosWithAuth.js
+++ mud/src/utils/axiosWithAuth.js
@@ -2,13 +2,12 @@ import axios from "axios";
 
 const axiosWithAuth = () => {
   const token = localStorage.getItem("token");
-  console.log("token", token);
 
   return axios.create({
     headers: {
       Authorization: "Token " + token
     },
-    baseURL: "https://lambda-mud-test.herokuapp.com" // Lambda MUD Test Server
+    baseURL: "https://adventure14.herokuapp.com" // Lambda MUD Test Server
   });
 };
 
